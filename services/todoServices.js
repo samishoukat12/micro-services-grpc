15d3b51f5b1c75ff253ccb611f975c39e789fc72
samishoukat12 <samishoukat12@gmail.com>
2025-09-09 13:25:23 +0500

    feat(todo): add delete todo rpc and improve update todo handling
    Add DeleteTodo RPC to proto definition and implement error handling in UpdateTodo service

--- services/todoServices.js
+++ services/todoServices.js
@@ -32,21 +32,34 @@ const TodoServices = {
         callback(null, { message: `Task deleted: ${call.request.id}` });
     },
     UpdateTodo: async (call, callback) => {
-        const updatedTask = await prisma.task.update({
+        const task = await prisma.task.findFirst({
             where: {
-                id: call.request.id,
-            },
-            data: {
-                task: call.request.task,
+                id: call.request.id
             }
         })
-        const response = {
-            id: updatedTask.id,
-            task: updatedTask.task,
-            createdAt: updatedTask.createdAt.toISOString(),
-            updatedAt: new Date().toISOString(), // Prisma won't auto-update unless you have updatedAt column
-        };
-        callback(null, response);
+        if (!task) {
+            callback(null, { message: `Task not found with id: ${call.request.id}`, status: 404 });
+            return;
+        }
+        else {
+            const updatedTask = await prisma.task.update({
+                where: {
+                    id: call.request.id,
+                },
+                data: {
+                    task: call.request.task,
+                }
+            })
+            const response = {
+                id: updatedTask.id,
+                task: updatedTask.task,
+                createdAt: updatedTask.createdAt.toISOString(),
+                updatedAt: new Date().toISOString(), // Prisma won't auto-update unless you have updatedAt column
+                message: "Task updated successfully",
+                status: 200
+            };
+            callback(null, response);
+        }
     }
 
 }
